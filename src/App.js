a8d1a858d880ff9fa63e5ce1f4eeb1f996c286b2
mopemope-Leaf <ulaqingdan@gmail.com>
2021-10-18 01:05:46 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { Button,Input, } from '@geist-ui/react'
 import { ethers } from 'ethers'
-import {OneInchApi} from "1inch-transaction";
+// import {OneInchApi} from "1inch-transaction";
+import {OneInchLimitApi, OneInchApi} from "1inch-transaction";
 const { ethereum } = window;
 
 
@@ -37,7 +38,8 @@ function App() {
 
 
     const  swapaction = async () => {
-        // let new1 = new OneInchApi();
+        let new1 = new OneInchLimitApi();
+        let new2 = new OneInchApi();
         // const {chainId} = await provider.getNetwork();
         // const eth_address ="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
         //
@@ -54,28 +56,56 @@ function App() {
         // // while (!approveResult) {
         // //     await delay(500);
         // // }
+
+        const fromToken = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        const toToken = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+        // const toToken = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        // const fromToken = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        const fromAmount = '90000000000000000000';
+        const toAmount = '110000000';
         // const fromToken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
-        // const toToken = '0xdac17f958d2ee523a2206206994597c13d831ec7';
-        // // new1.swap(
-        // //         chainId,
-        // //         fromToken,
-        // //         toToken,
-        // //         amount
-        // //     )
-        // //     .then((res) => {
-        // //         console.log('======swap', res);
-        // //     });
-        const params = {
-            from: '0x3CD18131FC5633705D6722B5D1fa88255BAF5d1F',
-            to: '0x11111112542d85b3ef69ae05771c2dccff4faa26',
-            data: '0x2e95b6c8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000470de4df82000000000000000000000000000000000000000000000000000396e77ca182222f530000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000180000000000000003b74a4607515be43d16f871588adc135d58a9c30a71eb34fcfee7c08',
-            value: '0x470DE4DF820000',
-        };
-        const txHash = await ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [params],
-        });
-        console.log(txHash);
+
+        // const fromAmount = '20000000000000000';
+        // const toAmount = '100000000';
+        const seconds = 7200;
+
+        // const params = await new1.approve(chainId, fromToken, fromAmount);
+        // const txHash = await ethereum.request({
+        //     method: 'eth_sendTransaction',
+        //     params: [params],
+        // });
+        // console.log(txHash);
+        new1.sign(
+                chainId,
+                fromToken,
+                toToken,
+                fromAmount,
+                toAmount,
+                seconds
+            )
+            .then((res) => {
+                console.log('======sign', res);
+                const [limitOrder, orderHash, orderSignature] = res;
+                new1.send(
+                    chainId,
+                    limitOrder,
+                    orderHash,
+                    orderSignature
+                ).then((res) => {
+                    console.log('======send', res);
+                })
+            });
+        // const params = {
+        //     from: '0x3CD18131FC5633705D6722B5D1fa88255BAF5d1F',
+        //     to: '0x11111112542d85b3ef69ae05771c2dccff4faa26',
+        //     data: '0x2e95b6c8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000470de4df82000000000000000000000000000000000000000000000000000396e77ca182222f530000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000180000000000000003b74a4607515be43d16f871588adc135d58a9c30a71eb34fcfee7c08',
+        //     value: '0x470DE4DF820000',
+        // };
+        // const txHash = await ethereum.request({
+        //     method: 'eth_sendTransaction',
+        //     params: [params],
+        // });
+        // console.log(txHash);
 
     }
 
